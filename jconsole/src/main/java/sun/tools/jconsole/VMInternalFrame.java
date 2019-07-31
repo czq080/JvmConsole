1735cb677614336d61b5b31ff64066bd44b8c37b
czq080 <czc808@hotmail.com>
2019-07-31 23:32:09 +0800

    init

---
+++ jconsole/src/main/java/sun/tools/jconsole/VMInternalFrame.java
@@ -0,0 +1,47 @@
+/*
+ * %W% %E%
+ *
+ * Copyright (c) 2006, Oracle and/or its affiliates. All rights reserved.
+ * ORACLE PROPRIETARY/CONFIDENTIAL. Use is subject to license terms.
+ */
+
+package sun.tools.jconsole;
+
+import javax.swing.*;
+import java.awt.*;
+
+import static sun.tools.jconsole.Resources.getText;
+import static sun.tools.jconsole.Utilities.setAccessibleDescription;
+
+@SuppressWarnings("serial")
+public class VMInternalFrame extends MaximizableInternalFrame {
+    private VMPanel vmPanel;
+
+    public VMInternalFrame(VMPanel vmPanel) {
+	super("", true, true, true, true);
+
+	this.vmPanel = vmPanel;
+	setAccessibleDescription(this,
+				 getText("VMInternalFrame.accessibleDescription"));
+	getContentPane().add(vmPanel, BorderLayout.CENTER);
+	pack();
+	vmPanel.updateFrameTitle();
+    }
+
+    public VMPanel getVMPanel() {
+	return vmPanel;
+    }
+
+    public Dimension getPreferredSize() {
+	Dimension d = super.getPreferredSize();
+	JDesktopPane desktop = getDesktopPane();
+	if (desktop != null) {
+	    Dimension desktopSize = desktop.getSize();
+	    if (desktopSize.width > 0 && desktopSize.height > 0) {
+		d.width  = Math.min(desktopSize.width  - 40, d.width);
+		d.height = Math.min(desktopSize.height - 40, d.height);
+	    }
+	}
+	return d;
+    }
+}
---
+++ jconsole/src/main/java/sun/tools/jconsole/inspector/XPlotter.java
@@ -0,0 +1,26 @@
+/*
+ * %W% %E%
+ *
+ * Copyright (c) 2006, Oracle and/or its affiliates. All rights reserved.
+ * ORACLE PROPRIETARY/CONFIDENTIAL. Use is subject to license terms.
+ */
+
+package sun.tools.jconsole.inspector;
+
+import sun.tools.jconsole.Plotter;
+
+import javax.swing.*;
+
+@SuppressWarnings("serial")
+public class XPlotter extends Plotter {
+    JTable table;
+    public XPlotter(JTable table, 
+		    Plotter.Unit unit) {
+	super(unit);
+	this.table = table;
+    }
+    public void addValues(long time, long... values) {
+	super.addValues(time, values);
+	table.repaint();	
+    }
+}
