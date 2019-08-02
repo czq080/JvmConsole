1eea00cd7f026112969aef2598b976ee803b799e
czq080 <chenzhiqiang@doctorwork.com>
2019-08-02 10:20:02 +0800

    打包后格式有誤

--- jconsole/src/main/script/start.bat
+++ jconsole/src/main/script/start.bat
@@ -5,7 +5,7 @@ cd ..\lib
 for %%i in (*) do set LIB_JARS=!LIB_JARS!;..\lib\%%i
 cd ..\bin
 
-java -Xms64m -Xmx1024m -XX:MaxPermSize=64M -classpath ..\conf;%LIB_JARS% sun.tools.jconsole.JConsole
+java -Xms64m -Xmx1024m -XX:MaxPermSize=64M -classpath %LIB_JARS% sun.tools.jconsole.JConsole
 goto end
 
 :end
